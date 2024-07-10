22507bdfd02c4ac6d71c0ecb0caedc4cc434d1f6
KIGMAN <Valentine Kiguli>
2024-07-09 20:35:30 -0700

    Added test cases

--- test/fibonacciTest.js
+++ test/fibonacciTest.js
@@ -6,5 +6,17 @@ describe('Fibonacci sequencer', function(){
     it('Fibonacci sequence debunked?', function(){
         assert.equal(fibonacci(1), 0, 'Returns the nth no. in the Fibonacci sequence')
     });
+    it('Fibonacci sequence debunked?', function(){
+        assert.equal(fibonacci(2), 1, 'Returns the nth no. in the Fibonacci sequence')
+    });
+    it('Fibonacci sequence debunked?', function(){
+        assert.equal(fibonacci(9), 21, 'Returns the nth no. in the Fibonacci sequence')
+    });
+    it('Fibonacci sequence debunked?', function(){
+        assert.equal(fibonacci(-1), 0, 'Returns the nth no. in the Fibonacci sequence')
+    });
+    it('Fibonacci sequence debunked?', function(){
+        assert.equal(fibonacci(1480), 0, 'Returns the nth no. in the Fibonacci sequence')
+    });
 });
 
